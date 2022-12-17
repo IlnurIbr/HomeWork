dd433957947437aa3b2d3bf76e348977ddcc03fb
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-18 02:11:25 +0500

    Определение суммы нечетных в массиве

---
+++ Task_15/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int[] NewArray(int amount, int a, int b) 
+{
+int[] array = new int[amount];
+for (int index = 0; index < amount; index++)
+{
+    array[index] = new Random().Next(a, b);
+}
+return array;
+}
+
+int SumNumber(int[] array)
+{
+    int sum = 0;
+for (int index = 0; index < array.Length;  index++)
+{
+    if (array[index]%2 != 0)
+    {
+        sum = sum + array[index];
+    }
+}
+return sum;
+}
+
+int [] array = NewArray(10, -100, 100);
+Console.Write($"[{string.Join(",",array)}]");
+int EvenNumbers = SumNumber(array);
+Console.Write($" --> {EvenNumbers} сумма нечетных в массиве");
