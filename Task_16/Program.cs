fad6afd8f3b62c864767c0a04adcd607ed4d5635
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-18 03:22:00 +0500

    Определение разницы между максимальным и минимальным числом

---
+++ Task_16/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+double[] NewArray(int amount, int a, int b) 
+{
+double[] array = new double[amount];
+for (int index = 0; index < amount; index++)
+{
+    array[index] = new Random().Next(a, b);
+}
+return array;
+}
+
+double Max(double[] array)
+{
+   double max = array[0];
+   for (int index = 1; index < array.Length;  index++)
+{
+    if (array[index] > max)
+    {
+        max = array[index];
+    }
+}
+return max;
+}
+
+double Min(double[] array)
+{
+    double min = array[0];
+for (int index = 0; index < array.Length;  index++)
+{
+    if (array[index] < min)
+    {
+        min = array[index];
+    }
+}
+return min;
+}
+
+double [] array = NewArray(10, -10, 10);
+Console.Write($"[{string.Join(",",array)}]");
+double DiffMaxMin = Max(array) - Min(array);
+Console.Write($" --> {DiffMaxMin} разница между максимальным и минимальным элементом");
