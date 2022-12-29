560d74ee81d6c82ee2d3be9701cfd5032031f153
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-30 01:12:43 +0500

    двумерный массив из случайных вещественных чисел

---
+++ Task_47/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 47: Задайте двумерный массив размером mхn заполненный случайными 
+вещественными числами*/
+
+void PrintArray(double[,] arr)
+{
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");;
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+double[,] FillArray(int rows, int columns, double begin, double end)
+{
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(0, 10));
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+
+//Запрос количества строк
+int rows = EnterData("Введите количество строк: ");
+//Запрос количества столбцов
+int columns = EnterData("Введите количество столбцов: ");
+//Создаем двумерный массив и заполняем его случайными числами
+double[,] matrix = FillArray(rows, columns, 0, 10);
+//ВВодим на экран
+PrintArray(matrix);
