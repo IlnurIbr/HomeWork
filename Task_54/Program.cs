fc448942479fe4abb5d79ec0d5d5d5a6cc16be1e
Ilnur Ibragimov <ilnur7081991@mail.ru>
2023-01-15 01:53:46 +0500

    Сортировка двумерного массива по убыванию

---
+++ Task_54/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыанию элемент каждой строки двумерного массива*/
+
+int[,] SortingArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(begin, end + 1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//Запрос количества строк
+int rows = EnterData("Введите количество строк: ");
+//Запрос количества столбцов
+int columns = EnterData("Введите количество столбцов: ");
+//Создаем двумерный массив и заполняем его случайными числами
+int[,] matrix = FillArray(rows, columns, 0, 9);
+//ВВодим на экран
+PrintArray(matrix);
+int[,] array = SortingArray(matrix);
+Console.WriteLine();
+PrintArray(array);
+
+
