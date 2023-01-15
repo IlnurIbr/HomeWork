f5fc500355c095c25f04a53b86c3e17127dde587
Ilnur Ibragimov <ilnur7081991@mail.ru>
2023-01-15 12:32:48 +0500

    Определение строки с наименьшей суммой элементов

---
+++ Task_56/Program.cs
@@ -0,0 +1,82 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка*/
+
+void SortingArray(int[,] array)
+
+{
+    
+int minSum = 0;
+int imin = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minSum = minSum + array[0, i];
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.WriteLine($"Cумма {i + 1} строки = {sum} ");   
+        if (sum <= minSum)
+        {
+           minSum = sum;
+           imin = i; 
+        }  
+          
+    }
+    Console.WriteLine($"строка с нименьшей суммой элементов - {imin+1}");
+     
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(begin, end + 1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//Запрос количества строк
+int rows = EnterData("Введите количество строк: ");
+//Запрос количества столбцов
+int columns = EnterData("Введите количество столбцов: ");
+//Создаем двумерный массив и заполняем его случайными числами
+int[,] matrix = FillArray(rows, columns, 0, 9);
+//ВВодим на экран
+PrintArray(matrix);
+SortingArray(matrix);
